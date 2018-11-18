ac13d4effe82e34c06b22645f445e82ab0d47d55
polytope <polytope@example.com>
2018-11-18 14:56:08 -0500

    Emulate freuk's git configuration (i.e., put spec in an /etc file).

--- git/default.nix
+++ git/default.nix
@@ -1,17 +1,19 @@
 { config, lib, pkgs, ... }:
 let
-  gitConfig = pkgs.writeScriptBin "gitConfig" ''
-    #!${pkgs.bash}/bin/bash
-    ${pkgs.git}/bin/git config --global user.name "polytope"
-    ${pkgs.git}/bin/git config --global user.email "polytope@example.com"
-    ${pkgs.git}/bin/git config --global alias.br branch
-    ${pkgs.git}/bin/git config --global alias.ci commit
-    ${pkgs.git}/bin/git config --global alias.co checkout
-    ${pkgs.git}/bin/git config --global alias.dump 'cat-file -p'
-    ${pkgs.git}/bin/git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
-    ${pkgs.git}/bin/git config --global alias.st status
-    ${pkgs.git}/bin/git config --global alias.type 'cat-file -t'
-    ${pkgs.git}/bin/git config --global core.excludesFile ${./ignore}
+  gitConfig = pkgs.writeText "gitConfig" ''
+    [user]
+      email = polytope@example.com
+      name = polytope
+    [alias]
+      br = branch
+      ci = commit
+      co = checkout
+      dump = cat-file -p
+      hist = log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short
+      st = status
+      type = cat-file -t
+    [core]
+      excludesFile = ${./ignore}
   '';
 in
   {
@@ -20,6 +22,7 @@ in
     };
 
     config = lib.mkIf config.environment.theo.programs.git.enable {
+      environment.etc."gitconfig".source = gitConfig;
       environment.shellAliases = {
         g = "git";
         ga = "git add";
@@ -36,7 +39,6 @@ in
           pkgs.gitAndTools.git-annex # manage files without committing file contents 
           pkgs.gitAndTools.git-crypt # encrypts particular files
           pkgs.gitAndTools.gitRemoteGcrypt # encrypts an entire repository
-          gitConfig
         ];
     };
   }
