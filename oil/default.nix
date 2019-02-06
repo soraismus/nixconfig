60a8b7f82b51d67a8c313cad6e4aafb12b849aae
polytope <polytope@example.com>
2019-02-06 14:31:36 -0500

    Make oil a shellPackage.

--- oil/default.nix
+++ oil/default.nix
@@ -10,6 +10,10 @@ stdenv.mkDerivation {
     sha256 = "3af4ca367dda26d2e67211db95a22d91e9840a3b18ad466c56458fa3b15af6a1";
   };
 
+  passthru = {
+    shellPath = "/bin/osh";
+  };
+
   postPatch = ''
     patchShebangs build
   '';
