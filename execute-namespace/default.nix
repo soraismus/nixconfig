a539eacc98740271c8645f8be7fce5abe7a8be71
soraismus <soraismus@example.com>
2019-03-24 20:05:54 -0400

    0. Escape bash-relevant characters in 'execute-namespace'; 1. refactor 'namespaces/note'.

--- execute-namespace/default.nix
+++ execute-namespace/default.nix
@@ -13,11 +13,16 @@ let
       fn=$1
       shift
       args="$@"
+      args="${args[@]//";"/"\\;"}" # Escape all semicolons in "$args[@]".
+      args="${args[@]//"&"/"\\&"}" # Escape all ampersands in "$args[@]".
+      args="${args[@]//"|"/"\\|"}" # Escape all pipes in "$args[@]".
+      args="${args[@]//">"/"\\>"}" # Escape all greater-than signs in "$args[@]".
+      args="${args[@]//"<"/"\\<"}" # Escape all less-than signs in "$args[@]".
 
       # Start a subshell, in which all of the functions of ...
       # ... the namespace $NAMESPACE can be loaded.
       # In the subshell, run the function $fn with args $args.
-      bash -c "source $NAMESPACES/$NAMESPACE; $fn $args"
+      bash -c "source ${NAMESPACES}/${NAMESPACE}; ${fn} ${args[@]}"
     '';
 in
 {
