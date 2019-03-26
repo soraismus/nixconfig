a13d464743ff98c6f5062fd07cee78bbd1877b6c
soraismus <soraismus@example.com>
2019-03-26 14:25:31 -0400

    0. Move definition of 'execute-namespace' to a separate file because nix syntax checking caused problems with 'nixos-rebuild'; 1. during process of character-escaping, use parentheses for bash arrays.

--- execute-namespace/default.nix
+++ execute-namespace/default.nix
@@ -3,26 +3,7 @@ let
   execute-namespace = pkgs.writeScriptBin "execute-namespace" ''
       #!${pkgs.bash}/bin/bash
 
-      # This script requires at least one argument (cmd).
-      # If 'cmd' doesn't exist, exit.
-      if [[ $# == 0 ]]; then
-        echo "  This script requires at least one argument (cmd, ...optional-args)"
-        exit 1;
-      fi
-
-      fn=$1
-      shift
-      args="$@"
-      args="${args[@]//";"/"\\;"}" # Escape all semicolons in "$args[@]".
-      args="${args[@]//"&"/"\\&"}" # Escape all ampersands in "$args[@]".
-      args="${args[@]//"|"/"\\|"}" # Escape all pipes in "$args[@]".
-      args="${args[@]//">"/"\\>"}" # Escape all greater-than signs in "$args[@]".
-      args="${args[@]//"<"/"\\<"}" # Escape all less-than signs in "$args[@]".
-
-      # Start a subshell, in which all of the functions of ...
-      # ... the namespace $NAMESPACE can be loaded.
-      # In the subshell, run the function $fn with args $args.
-      bash -c "source ${NAMESPACES}/${NAMESPACE}; ${fn} ${args[@]}"
+      ${builtins.readFile ./definition}
     '';
 in
 {
