4ba871c4708aaa2205c2e23148f8a76976c3982d
matthew-hilty <hilty.matthew@protonmail.com>
2022-08-23 12:53:00 -0400

    Add the vim plugin 'vim-crunch'.

--- vim/default.nix
+++ vim/default.nix
@@ -1,38 +1,54 @@
 { pkgs }:
-pkgs.vim_configurable.customize {
-  name = "vim";
-  vimrcConfig.customRC = builtins.readFile ./vimrc;
-  vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
-  vimrcConfig.vam.pluginDictionaries =
-    [ {
-        names =
-          [ "agda-vim"
-            # "ale"
-            "awesome-vim-colorschemes"
-            "fzf-vim"
-            "fzfWrapper"
-            "fugitive"
-            "gundo"
-            # "neoterm" # temporarily comment b/c of build issue
-            "syntastic"
-            "tabular"
-            "tagbar"
-            "ultisnips"
-            "vim-capslock"
-            "vim-commentary"
-            "vim-dadbod"
-            "vim-easymotion"
-            "vim-eunuch"
-            "vim-gitgutter"
-            "vim-unimpaired"
-            "vim-signature"
-            "vim-subversive"
-            "vim-surround"
-            # !gh machakan/vim-swap
-            "vim-tbone"
-            "vim-vinegar"
-            "vimtex"
-          ];
-      }
-    ];
-}
+let
+  mhVimPlugins = {
+    vim-crunch = pkgs.vimUtils.buildVimPluginFrom2Nix {
+      pname = "vim-crunch";
+      version = "2021-09-07";
+      src = pkgs.fetchFromGitHub {
+        owner = "arecarn";
+        repo = "vim-crunch";
+        rev = "2c99271108e0d86341837f185a6102363ccee8a6";
+        sha256 = "1qj5js3kpahslcj7vcwqmy19517hzqi4y8p710482w4k9w5ljrg6";
+      };
+      meta.homepage = "https://github.com/arecarn/vim-crunch/";
+    };
+  };
+in
+  pkgs.vim_configurable.customize {
+    name = "vim";
+    vimrcConfig.customRC = builtins.readFile ./vimrc;
+    vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // mhVimPlugins;
+    vimrcConfig.vam.pluginDictionaries =
+      [ {
+          names =
+            [ "agda-vim"
+              # "ale"
+              "awesome-vim-colorschemes"
+              "fzf-vim"
+              "fzfWrapper"
+              "fugitive"
+              "gundo"
+              # "neoterm" # temporarily comment b/c of build issue
+              "syntastic"
+              "tabular"
+              "tagbar"
+              "ultisnips"
+              "vim-capslock"
+              "vim-commentary"
+              "vim-crunch"
+              "vim-dadbod"
+              "vim-easymotion"
+              "vim-eunuch"
+              "vim-gitgutter"
+              "vim-unimpaired"
+              "vim-signature"
+              "vim-subversive"
+              "vim-surround"
+              # !gh machakan/vim-swap
+              "vim-tbone"
+              "vim-vinegar"
+              "vimtex"
+            ];
+        }
+      ];
+  }
