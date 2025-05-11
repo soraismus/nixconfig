74f86cf594d388453d3475f658c14afcf23de3db
matthew-hilty <hilty.matthew@protonmail.com>
2025-05-11 15:39:26 -0400

    Add the systemPackage game, 'pychess'.

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,13 @@
       # http://fontforge.github.io/en-US/documentation/utilities/
       # Tools include showttf, ttf2eps, pfadecrypt, pcl2ttf.
       fonttools = pkgs.fontforge-fonttools;
+
+      pinnedPkgs = import (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/b3582c75c7f21ce0b429898980eddbbf05c68e55.tar.gz";
+        sha256 = "1vf26scmz0826g49mqclmm4pblk5gzw5d4bk9bwql0psz916ij0n";
+      }) {};
+
+      pychess = pinnedPkgs.pychess; # GTK chess client
     in
       with pkgs; [
         # Consider:
@@ -101,6 +108,7 @@
         openssh # implementation of the SSH protocol
         parallel # shell tool fro executing jobs in parallel [cf. xargs ?]
         pass # password-store manages passwords securely
+        pychess # GTK chess client
         renameutils # a set of programs to make renaming of files faster
 
         (rstudioWrapper.override {
